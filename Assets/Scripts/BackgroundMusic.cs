c5944e5f66052eb49b154f61471d6e2bfd696f97
Hugh Riddle <hughriddle@YouRiddle.local>
2024-09-19 13:34:40 +1000

    Finished Audio Assets 35% - Z

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    public AudioSource currentlyPlaying;
+    public AudioClip levelStart;
+    public AudioClip normalState;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentlyPlaying.clip = levelStart;
+        currentlyPlaying.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!currentlyPlaying.isPlaying && currentlyPlaying.clip != normalState)
+        {
+            currentlyPlaying.clip = normalState;
+            currentlyPlaying.Play();
+        }
+    }
+}
