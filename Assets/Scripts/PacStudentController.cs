bbc9b0addf450880588e05de5a8e3d114f120a7f
Hugh Riddle <hughriddle@192-168-1-103.tpgi.com.au>
2024-11-03 06:56:34 +1100

    Added PacStudent movement input and lerping with collision

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PacStudentController : MonoBehaviour
 {
@@ -8,14 +10,17 @@ public class PacStudentController : MonoBehaviour
     public Animator animator;
     public AudioSource audioSource;
     public Grid grid;
+    public Tilemap tilemap;
 
     private float duration;
 
     private Transform playerTrans;
-    private KeyCode lastInput;
-    private KeyCode currentInput;
+    private Vector3Int startPos;
+    private Vector3Int lastInput;
+    private Vector3Int currentInput;
     private Vector3 cellOffset;
     private bool lerping;
+    private Coroutine currentRoutine;
     
     // private List<Tween> activeTweens;
     // private int tweenIndex;
@@ -26,14 +31,11 @@ public class PacStudentController : MonoBehaviour
     {
         playerTrans = gameObject.transform;
         duration = 0.2f;
+        lerping = false;
         
         cellOffset = new Vector3(0.5f, 0.5f, 0);
-        Vector3 startPos = grid.CellToWorld(new Vector3Int(-11, 1, 0)) + cellOffset;
-        playerTrans.position = startPos;
-        
-        Vector3 endPos = grid.CellToWorld(new Vector3Int(-10, 1, 0)) + cellOffset;
-        MovePlayer(endPos);
-        
+        startPos = new Vector3Int(-11, 1, 0);
+        playerTrans.position = grid.CellToWorld(startPos) + cellOffset;
     }
 
     // Update is called once per frame
@@ -41,44 +43,73 @@ public class PacStudentController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            lastInput = KeyCode.W;
+            lastInput = Vector3Int.up;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            lastInput = KeyCode.A;
+            lastInput = Vector3Int.left;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            lastInput = KeyCode.S;
+            lastInput = Vector3Int.down;
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            lastInput = KeyCode.D;
+            lastInput = Vector3Int.right;
+        }
+
+        if (!lerping && lastInput != null)
+        {
+            MovePlayer();
         }
     }
 
-    private void MovePlayer(Vector3 targetPos)
+    private void MovePlayer()
     {
-        lerping = true;
-        StartCoroutine(LerpPlayer(targetPos));
+        Vector3Int targetPos;
+        Vector3 tarWorldPos;
+        
+        if (lastInput != currentInput)
+        {
+            targetPos = startPos + lastInput;
+            tarWorldPos = grid.CellToWorld(targetPos) + cellOffset;
+            
+            if (tilemap.GetTile(targetPos) is null)
+            { 
+                currentInput = lastInput;
+                startPos = targetPos;
+                lerping = true;
+                StartCoroutine(LerpPlayer(tarWorldPos));
+                return;
+            }
+        }
+        
+        targetPos = startPos + currentInput;
+        tarWorldPos = grid.CellToWorld(targetPos) + cellOffset;
+
+        if (tilemap.GetTile(targetPos) is null)
+        {
+            startPos = targetPos;
+            lerping = true;
+            StartCoroutine(LerpPlayer(tarWorldPos));
+        }
     }
     
-    private IEnumerator LerpPlayer(Vector3 targetPos)
+    private IEnumerator LerpPlayer(Vector3 target)
     {
         Vector3 startingPos = playerTrans.position;
         float currentTime = 0.0f;
         while (currentTime < duration)
         {
-            playerTrans.position = Vector3.Lerp(startingPos, targetPos, currentTime / duration);
+            playerTrans.position = Vector3.Lerp(startingPos, target, currentTime / duration);
             currentTime += Time.deltaTime;
             yield return null;
         }
-
-        yield return null;
-        playerTrans.position = targetPos;
+        
+        playerTrans.position = target;
         lerping = false;
     }
 }
